0266aca8ec3816d77abc53a9252fbb41fd333cab
Dzebliuk Oleksandr <dzeblyuk@gmail.com>
2017-11-27 08:14:43 +0200

    add variables

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -4,7 +4,5 @@ module.exports = [
   './node_modules/normalize.css/normalize.css',
   './node_modules/slick-carousel/slick/slick.css',
   './node_modules/slick-carousel/slick/slick-theme.css',
-  './node_modules/animate.css/animate.min.css',
-  './node_modules/aos/dist/aos.css'
-  /*'./node_modules/fullpage.js/dist/jquery.fullPage.css'*/
+  './node_modules/animate.css/animate.min.css'
 ];
--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -3,9 +3,6 @@
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/slick-carousel/slick/slick.min.js',
-  /*'./node_modules/particles.js/particles.js',*/
-  './source/js/jquery.viewportchecker.js',
-  './node_modules/aos/dist/aos.js'
-  /*'./node_modules/fullpage.js/vendors/scrolloverflow.min.js',*/
-  /*'./node_modules/fullpage.js/dist/jquery.fullPage.js'*/
+  /* './source/js/jquery.viewportchecker.js', */
+  /* './node_modules/mobile-detect/mobile-detect.min.js' */
 ];
